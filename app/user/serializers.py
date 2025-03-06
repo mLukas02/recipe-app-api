12602f1a5a9ea886c5a72896682c56d7a1345812
Mikhail Lukashevich <mikhail.n.lukashevich@gmail.com>
2025-03-06 15:17:42 +0100

    Feat(user): Implement User Manager API and add related tests.

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -3,8 +3,10 @@ Serializers for the user API View.
 """
 from django.contrib.auth import (
     get_user_model,
+    authenticate,
 )
 from rest_framework import serializers
+from django.utils.translation import gettext as _
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -29,3 +31,29 @@ class UserSerializer(serializers.ModelSerializer):
             user.save()
 
         return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user auth token."""
+
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={"input_type": "password"},
+        trim_whitespace=False,
+    )
+
+    def validate(self, attrs):
+        """Validate and authenticate the user."""
+        email = attrs.get("email")
+        password = attrs.get("password")
+        user = authenticate(
+            request=self.context.get("request"),
+            username=email,
+            password=password,
+        )
+        if not user:
+            msg = _("Unable to authenticate with provided credentials.")
+            raise serializers.ValidationError(msg, code="authorization")
+
+        attrs["user"] = user
+        return attrs
